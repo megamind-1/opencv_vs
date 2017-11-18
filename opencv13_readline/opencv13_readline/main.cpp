71d19d92652e17bb4cbfaadc4badb7f426baa7fb
Not found 404 <1665883518@qq.com>
2017-11-18 15:31:34 +0800

    13u

--- opencv13_readline/opencv13_readline/main.cpp
+++ opencv13_readline/opencv13_readline/main.cpp
@@ -6,7 +6,7 @@ using namespace cv;
 int main()
 {
 	Mat src;
-	src = imread("D:/pics/source/test1.jpg");				//输入彩色图像
+	src = imread("D:/pics/source/test7.jpg");				//输入彩色图像
 	if (src.empty()) {
 		cout << "can't open!" << endl;
 		return -1;
@@ -23,18 +23,21 @@ int main()
 	adaptiveThreshold(gray_src, binimg, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY, 15, -2);
 	namedWindow("bin", CV_WINDOW_AUTOSIZE);
 	imshow("bin", binimg);
-	//imwrite("D:/pics/source/gray3.jpg", binimg);
-
 															//水平结构元素
 	Mat hline = getStructuringElement(MORPH_RECT, Size(src.cols/16,	1), Point(-1, -1));
 															//竖直结构元素
 	Mat vline = getStructuringElement(MORPH_RECT, Size(1, src.rows/16), Point(-1, -1));
+															//矩形结构元素
+	Mat kernel = getStructuringElement(MORPH_RECT, Size(3, 3), Point(-1, -1));
 
 	Mat temp;
 	Mat dst;
-	erode(binimg, temp, vline);								//腐蚀
-	dilate(temp, dst, vline);								//膨胀
+	erode(binimg, temp, kernel);								//腐蚀
+	dilate(temp, dst, kernel);									//膨胀
+	//morphologyEx(binimg, dst, CV_MOP_OPEN, vline);				//开操作 = 腐蚀 + 膨胀
+
 	bitwise_not(dst, dst);									//变成白色
+	blur(dst, dst, Size(3, 3), Point(-1, -1));
 	imshow("temp", dst);
 
 	waitKey(0);
